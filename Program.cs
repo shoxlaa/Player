aeb71fdc8cd1ad55fd33c75d361e03c619d13b4b
shoxlaa <shahla.guliyeva02@gmail.com>
2022-01-04 13:26:56 +0400

    final

--- Program.cs
+++ Program.cs
@@ -6,18 +6,41 @@ using System.Collections.Generic;
 namespace ConsoleApp1
 {
 
-    enum Buff { }
-    enum Debuff { }
-    //new file 
-
-    class Player
-    {
-         public Point Coardinates { get; set; }
-        public uint Helth { get; set; }
+    enum Buff {Speed, Invisibility, Saturation  }
+    enum Debuff {Slownes, Blindness, Hunger }
+   
+    class Player   {
+       public Point Coardinates { get; set; }
+        public int Helth { get; set; }
         public bool IsAlive { get; set; }
 
-        List<Buff> Buffs = new List<Buff>();
-        List<Debuff> Debuffs = new List<Debuff>();
+       public List<Buff> Buffs = new List<Buff>();
+       public List<Debuff> Debuffs = new List<Debuff>();
+
+        public static int operator+ (Player left)
+        {
+            if(left.Helth<100 && left.Helth+10<=100)
+            { 
+            left.Helth+=10; 
+            }
+            return left.Helth;
+        } 
+        public static int operator- (Player left)
+        {
+            if(left.Helth>0 && left.Helth-10>=0)
+            { 
+            left.Helth-=10; 
+            }
+            return left.Helth;
+        }
+
+        public void AddBuff(Buff buff)
+        {Buffs.Add(buff);}
+
+        public void AddDebuff(Debuff  debuff)
+        {  Debuffs.Add(debuff);} 
+        public void RemoveBuff(Buff buff) {Buffs.Remove(buff);} 
+        public void RemoveDebuff(Debuff  debuff) {Buffs.Remove(debuff); }
 
     }
     class Point
@@ -53,8 +76,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            Player Kek= new Player(); 
+            Player Ke= new Player(); 
 
+          int valu = +Ke;
+            valu = +Ke;
+            valu=-Ke;
+           
+           
 
+            Console.WriteLine(valu); 
+
+            Console.ReadLine();
+             
         }
     }
 }
